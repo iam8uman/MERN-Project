e38d648d0401860f2820270e4a0b8fc62b603a67
iamsuman <suman.191544@ncit.edu.np>
2023-09-24 11:45:24 +0545

    File Update

--- E-commerse/frontend/src/App.jsx
+++ E-commerse/frontend/src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1 className="header text-3xl">Hello React Header</h1>
+    <h1>Hello Nepal!</h1>
+    <Navbar/>
+    <Home/>
+    <Footer/>
     </>
   )
 }
