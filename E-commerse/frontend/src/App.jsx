78c9e5a68c2d3f034aa57a7026e34f497bd0dac5
iamsuman <suman.191544@ncit.edu.np>
2023-09-25 23:48:25 +0545

    Updated and Understand

--- E-commerse/frontend/src/App.jsx
+++ E-commerse/frontend/src/App.jsx
@@ -1,31 +1,26 @@
-import { Outlet, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
 import Products from "./components/Product";
 import AddProduct from "./components/Add";
 import UpdateProduct from "./components/UpdateProduct";
 import Profile from "./components/Profile";
 import Logout from "./components/Logout";
 import Signup from "./components/Signup";
-
+import Root from "./routes/root";
 
 function App() {
   return (
     <>
-      <Navbar />
-
-      {/* <Outlet /> */}
-
+      {/* <Outlet vitra ko kura matra xa yeha /> */}
       <Routes>
+        <Route path="/" element={<Root />}>
           <Route path="products" element={<Products />} />
           <Route path="add" element={<AddProduct />} />
           <Route path="update" element={<UpdateProduct />} />
           <Route path="logout" element={<Logout />} />
           <Route path="profile" element={<Profile />} />
           <Route path="signup" element={<Signup />} />
+        </Route>
       </Routes>
-
-      <Footer />
     </>
   );
 }
