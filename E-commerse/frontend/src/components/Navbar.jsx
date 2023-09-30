4836088f76f47042049da8fc3e01c4dc5ccd7b0a
iamsuman <suman.191544@ncit.edu.np>
2023-09-30 16:19:16 +0545

    mongodb databse connection + get and post api

--- E-commerse/frontend/src/components/Navbar.jsx
+++ E-commerse/frontend/src/components/Navbar.jsx
@@ -40,7 +40,7 @@ export default function Navbar() {
                     block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                   }
                 >
-                  Product
+                  Products
                 </NavLink>
               </li>
               <li>
@@ -87,7 +87,6 @@ export default function Navbar() {
                   Logout
                 </NavLink>
               </li>
-              
             </ul>
           </div>
         </div>
