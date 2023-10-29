b7b39fe634f99e1198da2ebadf1b2630cdbd4bdd
iamsuman <suman.191544@ncit.edu.np>
2023-10-29 19:16:52 +0545

    a mega project of blog with appwrite

---
+++ Typescripttt/TypeOne/firstType.ts
@@ -0,0 +1,29 @@
+// let count= "hellovaisab"
+
+// count = 1;
+
+// count = true;
+
+// console.log(count)
+
+
+// interface User{
+//     name:string,
+//     age:number
+// }
+
+// const user:User={
+//     name:"Suman Sharma",
+//     age:22,
+// }
+
+
+// console.log(user)
+
+// const msg="hello world"
+
+// const Upper = msg.toLowerCase()
+// console.log(Upper)
+
+let Ktakti= 5;
+Ktakti="halla nagar"
