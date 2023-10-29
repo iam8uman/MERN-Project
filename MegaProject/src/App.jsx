8552193f05a864c32c655c79867a7def7b9bedae
iamsuman <suman.191544@ncit.edu.np>
2023-10-29 19:29:40 +0545

    vite env file setup and working fine

--- MegaProject/src/App.jsx
+++ MegaProject/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 
 function App() {
+  console.log(import.meta.env.VITE_Appwrite_URL)
 
   return (
     <>
+
        <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
