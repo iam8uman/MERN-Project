32459cde6faccb690e06b199749222d8702d5849
iamsuman <suman.191544@ncit.edu.np>
2023-11-02 21:24:12 +0545

    redux done, appwrite done, now components turn

--- MegaProject/src/App.jsx
+++ MegaProject/src/App.jsx
@@ -1,15 +1,43 @@
 import "./App.css";
-import config from "./config/config";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
+import authService from "./appwrite/auth";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(config.appwriteDBID);
-  console.log(config.appwritePID);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch(); // state change garna use hunxa!
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (!loading) {
+    return (
+      <div className="min-h-screen flex flex-wrap content-between bg-grey-400">
+        <div className="w-full bg-gray-100">
+          <Header />
+          <main> Outlettt::<Outlet/></main>
+          <Footer />
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default App;
