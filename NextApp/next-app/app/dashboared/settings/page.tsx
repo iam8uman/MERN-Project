d1f1809e48717baeaff098e5cad13f4427953e81
iamsuman <suman.191544@ncit.edu.np>
2023-11-04 22:49:41 +0545

    Next Routing almost complete

--- NextApp/next-app/app/dashboared/settings/page.tsx
+++ NextApp/next-app/app/dashboared/settings/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div>
-      <h2>Dashboarddd</h2>
+      <h2 className='bg-slate-400 m-2 p-2 rounded-md text-white'>Dashboarddd <br /> Love to see that i actually learn routing in next</h2>
     </div>
   )
 }
