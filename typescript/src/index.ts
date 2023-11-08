1efff6938b532eeb2909709cb0caffd13277ade5
iamsuman <suman.191544@ncit.edu.np>
2023-11-08 13:01:13 +0545

    Debugging i tried this time

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,2 +1,12 @@
-let value : string = "Hello World";
-console.log(value);
+// let value : string = "Hello World";
+// console.log(value);
+// 
+let age:number =20;
+
+if(age>18){
+    console.log("Eligible for voting", age);
+}
+    else{
+        age+=25;
+        console.log(age)
+    }
