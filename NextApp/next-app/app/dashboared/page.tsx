d18a9e6126d3006cff5cd96b29358d4e9215cafa
iamsuman <suman.191544@ncit.edu.np>
2023-11-09 21:48:37 +0545

    Update on Link & Navigation

--- NextApp/next-app/app/dashboared/page.tsx
+++ NextApp/next-app/app/dashboared/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -7,6 +8,9 @@ export default function Home() {
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
+          <Link href="/dashboared/page.tsx">
+          <div className="button m-2 rounded-md">Hello World!</div>
+          </Link>
         </p>
       </div>
     </main>
--- NextApp/next-app/app/dashboared/settings/page.tsx
+++ NextApp/next-app/app/dashboared/settings/page.tsx
@@ -1,9 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Dashboard = () => {
   return (
     <div>
       <h2 className='bg-slate-400 m-2 p-2 rounded-md text-white'>Dashboarddd <br /> Love to see that i actually learn routing in next</h2>
+      <Link href='/Dash'>
+        <div className='button m-2 rounded-md'>Go to Home</div>
+        </Link>
+
+        {/* post lai dekhaune kam gara */}
+        <div className='flex flex-wrap'>
+          <Link href='/' className='bg-purple-500 m-3 text-2xl p-2 rounded-md md:text-1xl'>Home</Link>
+          <Link href='/dashboared' className='bg-purple-500 m-3 text-2xl p-2 rounded-md md:text-1xl'>Dashboared</Link>
+          <Link href='/dashboared/settings' className='bg-purple-500 m-3 text-2xl p-2 rounded-md md:text-1xl'>Settings</Link>
+          </div>
     </div>
   )
 }
