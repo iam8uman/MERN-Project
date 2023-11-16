03284c93c04b9ae1226e8bbfbefc411d6208c812
iamsuman <suman.191544@ncit.edu.np>
2023-11-16 18:23:25 +0545

    Data fetching in Next.js

--- NextApp/next2/src/app/page.tsx
+++ NextApp/next2/src/app/page.tsx
@@ -1,19 +1,42 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 
-export default function Home() {
-  return (
-   // data fetching in next.js
+// export default function Home() {
+//   return (
+//    // data fetching in next.js
 
-   async function getData() {
-    const res = await fetch('https://api.example.com/...')
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
+//    async function getData() {
+//     const res = await fetch('https://api.example.com/...')
+//     // The return value is *not* serialized
+//     // You can return Date, Map, Set, etc.
    
-    if (!res.ok) {
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error('Failed to fetch data')
+//     if (!res.ok) {
+//       // This will activate the closest `error.js` Error Boundary
+//       throw new Error('Failed to fetch data')
+//     }
+   
+//     return res.json()
+//   }
+//   )
+// }
+
+import Image from "next/image";
+function  Home(){
+ return(
+ 
+  async function  getData() {
+    const res = await fetch('https://api.example.com/posts');
+
+    if(res.ok)
+    {
+      return res.json();
     }
-   
-    return res.json()
+    else{
+      throw new Error('Failed to fetch data');
+    }
+    
   }
+   
+   )
 }
+
+export default Home;
