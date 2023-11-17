8fdf6f3bc9b0fc9e7e0a6be694f1cbcf034837b4
iamsuman <suman.191544@ncit.edu.np>
2023-11-17 18:58:37 +0545

    API endpoints tested and completed

--- NextApp/next2/src/app/api/admin/route.ts
+++ NextApp/next2/src/app/api/admin/route.ts
@@ -1,6 +1,14 @@
 export async function GET(request: Request) {
     // write backend logic here
-    
+
+    return new Response('Hello world from admin route!');
 
 
+}
+
+// post method
+export async function POST(request: Request) {
+    // write backend logic here
+
+    return new Response('Hello world from admin route! POST Method');
 }
---
+++ NextApp/next2/src/app/api/users/route.ts
@@ -0,0 +1,7 @@
+export async function GET(request: Request) {
+    // write backend logic here
+
+    return new Response('Hello world from User route! Get Methods');
+
+
+}
