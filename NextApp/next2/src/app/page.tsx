7d4ecb4437fb8a9c35aac21170e22a80c5e56fd5
iamsuman <suman.191544@ncit.edu.np>
2023-11-17 18:43:28 +0545

    Next.js API Endpoints

--- NextApp/next2/src/app/page.tsx
+++ NextApp/next2/src/app/page.tsx
@@ -8,35 +8,30 @@
 //     const res = await fetch('https://api.example.com/...')
 //     // The return value is *not* serialized
 //     // You can return Date, Map, Set, etc.
-   
+
 //     if (!res.ok) {
 //       // This will activate the closest `error.js` Error Boundary
 //       throw new Error('Failed to fetch data')
 //     }
-   
+
 //     return res.json()
 //   }
 //   )
 // }
 
 import Image from "next/image";
-function  Home(){
- return(
- 
-  async function  getData() {
-    const res = await fetch('https://api.example.com/posts');
-
-    if(res.ok)
-    {
-      return res.json();
-    }
-    else{
-      throw new Error('Failed to fetch data');
-    }
+function Home({ params }) {
+  return async function getData() {
+    const res = await fetch(`https://api.example.com/posts/${params.id}`, {
+      cache: "no-store",
+    });
     
-  }
-   
-   )
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error("Failed to fetch data");
+    }
+  };
 }
 
 export default Home;
