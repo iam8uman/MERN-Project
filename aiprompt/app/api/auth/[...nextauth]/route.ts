6e206f625b81485e158d986d7719d1337a94ec4e
iamsuman <suman.191544@ncit.edu.np>
2023-11-23 21:50:15 +0545

    next-auth

---
+++ aiprompt/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  secret: process.env.SECRET,
+  providers: [
+    // OAuth authentication providers
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+
+  async signIn({profile}) {
+    
+  },
+
+  async Session({ session }) {
+
+  }
+});
