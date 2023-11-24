e9c2269ef0f9876e4465905f9b0199bbcbc7e1a6
iamsuman <suman.191544@ncit.edu.np>
2023-11-24 23:16:50 +0545

    Todos completed

--- next-todos/app/layout.js
+++ next-todos/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'next-todos',
+  title: 'next-todos-Selfmade',
   description: 'Todo app built with Next.js',
 }
 
--- next-todos/app/page.js
+++ next-todos/app/page.js
@@ -1,83 +1,145 @@
+// 'use client'
+
+// import Image from "next/image";
+// import { useState } from "react";
+
+// export default function Home() {
+
+// const [todos,setTodos] = useState('');
+// const [todoList, setTodoList] = useState([]);
+
+
+// const addTodo = () => {
+
+//   if (todos === '') {
+//     alert('Please enter a todo');
+
+//   }
+//   if(todos!== ''){
+//     setTodoList([...todoList,todos]);
+//     setTodos('');
+//   }
+//   else{
+//     return null;
+//   }
+
+// }
+
+// // delete todo function
+// const deleteTodo = (index) => { 
+//   const newTodoList = [...todoList];
+//   newTodoList.splice(index,1);
+//   setTodoList(newTodoList);
+// }
+
+
+// // Update the todo function
+
+// const editTodo = (index) => {
+
+  
+// }
+
+
+
+
+//   return (
+//     <main className="m-3">
+//       <div className="flex flex-col items-center justify-center">
+//         <h1 className="text-5xl font-bold text-center">Next.js Todos</h1>
+//         <p className="text-center">Todo app built with Next.js</p>
+//       </div>
+
+//       {/* input files for the todos to entered */}
+//       <div className="flex justify-center">
+//         <input
+//           className="w-1/2 p-2 m-3 border-2 text-black border-gray-200 rounded-lg"
+//           type="text"
+//           placeholder="Add your todos here"
+//           onChange={(e) => setTodos(e.target.value)}
+//         />
+//         <button className="w-1/12 p-2 m-3 text-white bg-blue-500 rounded-lg" onClick={addTodo}>
+//           Add Todo
+//         </button>
+//       </div>
+
+//       {/* list of todos show*/}
+
+//       <div className="flex flex-col items-center justify-center">
+//         <ul>
+//           {todoList.map((todo) => (
+//             <li className="flex justify-start">
+//               <p className="p-2 m-3 w-full text-black bg-white rounded-lg">{todo}</p>
+//               {/* delete Button  */}
+//               <button className="p-2 m-3 text-white bg-red-500 rounded-lg" onClick={deleteTodo}>Delete</button>
+//               {/* edit button  */}
+//               <button className="p-2 m-3 text-white bg-green-500 rounded-lg">Edit</button>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </main>
+//   );
+// }
+
+
 'use client'
-
-import Image from "next/image";
-import { useState } from "react";
-
-export default function Home() {
-
-const [todos,setTodos] = useState('');
-const [todoList, setTodoList] = useState([]);
+import { useState } from 'react'
 
 
-const addTodo = () => {
+const page = () => {
+   const [todos, setTodos] = useState('')
+   const [todoList, setTodoList] = useState([])
 
-  if (todos === '') {
-    alert('Please enter a todo');
-
+   const addTodo = () => {
+    if(todos !== ''){
+    setTodoList([...todoList, todos])
+    setTodos('')
+    } else {
+      alert('Please enter a todo')
+    }
   }
-  if(todos!== ''){
-    setTodoList([...todoList,todos]);
-    setTodos('');
-  }
-  else{
-    return null;
-  }
-
-}
-
-// delete todo function
-const deleteTodo = (index) => { 
-  const newTodoList = [...todoList];
-  newTodoList.splice(index,1);
-  setTodoList(newTodoList);
-}
-
-
-// Update the todo function
-
-const editTodo = (index) => {
-  
-  
-}
-
-
-
 
   return (
-    <main className="m-3">
+    <>
+      {/* title next-todos */}
+
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-5xl font-bold text-center">Next.js Todos</h1>
+        <h1 className="m-5 p-2 text-5xl font-bold text-center">Next.js Todos</h1>
         <p className="text-center">Todo app built with Next.js</p>
+
+        {/* input fields with add button  */}
+        <div className="flex justify-center">
+          <input
+            className="max-w-screen-lg p-2 m-3 border-2 text-black border-gray-200 rounded-lg"
+            type="text"
+            placeholder="Add your todos here"
+            onChange={(e) => setTodos(e.target.value)}
+          />
+          <button onClick={addTodo} className="w-full p-2 m-3 text-white bg-blue-500 rounded-lg">
+            Add Todo
+          </button>
+          </div>
+
+          {/* // show the list of todos  */}
+          <div className='flex flex-col items-center justify-center'>
+            <ul>
+              {todoList.map((todo) => (
+                <li className="flex justify-start">
+                  <p className="p-2 m-3 w-full text-black bg-white rounded-lg">{todo}</p>
+                  {/* delete Button  */}
+                  <button className="p-2 m-3 text-white bg-red-500 rounded-lg">Delete</button>
+                  {/* edit button  */}
+                  <button className="p-2 m-3 text-white bg-green-500 rounded-lg">Edit</button>
+                </li>
+              ))}
+            </ul>
+            </div>  
       </div>
 
-      {/* input files for the todos to entered */}
-      <div className="flex justify-center">
-        <input
-          className="w-1/2 p-2 m-3 border-2 text-black border-gray-200 rounded-lg"
-          type="text"
-          placeholder="Add your todos here"
-          onChange={(e) => setTodos(e.target.value)}
-        />
-        <button className="w-1/12 p-2 m-3 text-white bg-blue-500 rounded-lg" onClick={addTodo}>
-          Add Todo
-        </button>
-      </div>
-
-      {/* list of todos show*/}
-
-      <div className="flex flex-col items-center justify-center">
-        <ul>
-          {todoList.map((todo) => (
-            <li className="flex justify-start">
-              <p className="p-2 m-3 w-full text-black bg-white rounded-lg">{todo}</p>
-              {/* delete Button  */}
-              <button className="p-2 m-3 text-white bg-red-500 rounded-lg" onClick={deleteTodo}>Delete</button>
-              {/* edit button  */}
-              <button className="p-2 m-3 text-white bg-green-500 rounded-lg">Edit</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </main>
-  );
+    </>
+  )
 }
+
+export default page
+
