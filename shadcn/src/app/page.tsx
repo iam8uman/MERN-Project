17f3f1ac977689b22b538866c57ea94696341fdf
iamsuman <suman.191544@ncit.edu.np>
2023-11-24 08:50:53 +0545

    Uptodate

--- shadcn/src/app/page.tsx
+++ shadcn/src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 export default function Home() {
   return (
     <div>
       <Button>Click me</Button>
+      <br />
+      <Input type="password" />
     </div>
   )
 }
