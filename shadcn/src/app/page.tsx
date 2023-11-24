1cd9694d84248c9ee7b9681b435336601cbcddfd
iamsuman <suman.191544@ncit.edu.np>
2023-11-24 08:54:24 +0545

    card component is added

--- shadcn/src/app/page.tsx
+++ shadcn/src/app/page.tsx
@@ -1,5 +1,13 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Home() {
   return (
@@ -7,6 +15,18 @@ export default function Home() {
       <Button>Click me</Button>
       <br />
       <Input type="password" />
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </div>
-  )
+  );
 }
