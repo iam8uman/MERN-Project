2585099a87d14c057612498830392813c081c483
iamsuman <suman.191544@ncit.edu.np>
2023-12-07 15:24:34 +0545

    API for next project

--- idea143/app/api/posts/route.ts
+++ idea143/app/api/posts/route.ts
@@ -1,3 +1,6 @@
+import { NextResponse } from "next/server";
+
+// GET method for the route /api/posts
 export async function GET() {
   // return 'Hello World!'
   const value=25;
@@ -10,4 +13,9 @@ export async function GET() {
   return Response.redirect('https://www.google.com');
     }
 
+}
+
+// POST method for the route /api/posts
+export async function POST() {
+  return NextResponse.json({msg: 'Hello World!'});
 }
